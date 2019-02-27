f4398f582a57dd9400bbaf9ae76b09656709bfaa
Daniel Montesomething <dmonte3210@gmail.com>
2019-02-26 22:42:03 -0500

    added backup bits and cleaned up wibbly wobbley timey wimey stuff

--- hash_slinging_sla.sh
+++ hash_slinging_sla.sh
@@ -4,43 +4,63 @@
 #Cleanup
 
 rm /var/log/anomalies.noot 2>/dev/null
+if [[ ! -d /var/tmp/tbin ]]; then mkdir /var/tmp/tbin; fi
+if [[ ! -d /var/tmp/tsbin ]]; then mkdir /var/tmp/tsbin; fi
 
-function noot_ify_bash() {
+function hackup_bashes() {
     rm /var/log/bkbin.log 2>/dev/null
     rm /var/log/bksbin.log 2>/dev/null
-    #intial file creation
-    for fname in `ls -F /usr/bin | grep '*'`
+    rm /var/tmp/tbin/* 2>/dev/null
+    rm /var/tmp/tsbin/* 2>/dev/null
+
+    for fname in $(ls -F /usr/bin | grep '*' | sed 's/.$//')
     do
+	cp /usr/bin/$fname /var/tmp/tbin
 	md5sum -b /usr/bin/$fname >> /var/log/bkbin.log 2>/dev/null
     done
-    for fname in `ls -F /usr/sbin | grep '*'`
+    for fname in $(ls -F /usr/sbin | grep '*' | sed 's/.$//')
     do	
+	cp /usr/sbin/$fname /var/tmp/tsbin
 	md5sum -b /usr/sbin/$fname >> /var/log/bksbin.log 2>/dev/null
     done
 }
 
-noot_ify_bash
+hackup_bashes
 
-#next thing
 while true
 do
     rm /var/tmp/curbin.txt 2>/dev/null
     rm /var/tmp/cursbin.txt 2>/dev/null
-    for fname in `ls -F /usr/bin | grep '*'`
+    for fname in $(ls -F /usr/bin | grep '*' | sed 's/.$//')
     do
 	md5sum -b /usr/bin/$fname >> /var/tmp/curbin.txt 2>/dev/null
     done
-    for fname in `ls -F /usr/sbin | grep '*'`
+    for fname in $(ls -F /usr/sbin | grep '*' | sed 's/.$//')
     do
 	md5sum -b /usr/sbin/$fname >> /var/tmp/cursbin.txt 2>/dev/null
     done
     check1="$(diff /var/log/bkbin.log /var/tmp/curbin.txt)"
     check2="$(diff /var/log/bksbin.log /var/tmp/cursbin.txt)"
-    if [[ ! -z $check1 ]] || [[ ! -z $check2 ]]; then
-	printf "\x1B[31m$check1$check2\x1B[0m\n"
+    anoms=0
+    if [[ -n $check1 ]]; then
+	anomaly="$(echo $check1 | grep -Eo '(\/usr[^ ]*) ')"
+	printf "\x1B[31mAnomaly Found in %s\n" $anomaly
+	anofname="$(echo $anomaly | rev | grep -Eo '(^[^/]*)/'| rev)"
+	anofname=${anofname:1:$((${#anofname}-1))}
+	printf "Reverting $anomaly from /var/tmp/tbin/$anofname\x1B[0m\n"
+	cp /var/tmp/tbin/$anofname $anomaly
 	echo $check1 >> /var/log/anomalies.noot
-	echo $check2 >> /var/log/anomalies.noot
-	noot_ify_bash
+	((anoms++))
     fi
-    sleep 10
+    if [[ -n $check2 ]]; then
+	anomaly="$(echo $check2 | grep -Eo '(\/usr[^ ]*) ')"
+	printf "\x1B[31mAnomaly Found in %s\n" $anomaly
+	anofname="$(echo $anomaly | rev | grep -Eo '(^[^/]*)/'| rev)"
+	anofname=${anofname:1:$((${#anofname}-1))}
+	printf "Reverting $anomaly from /var/tmp/tsbin/$anofname\x1B[0m\n"
+	cp /var/tmp/tsbin/$anofname $anomaly
+	echo $check2 >> /var/log/anomalies.noot
+	((anoms++))
+    fi
+    if [[ anoms -gt 0 ]]; then hackup_bashes; fi
 done
